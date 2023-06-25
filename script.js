4eb0b7a6dcc9668959ad133247e1514eac6c86cc
SoulmongerV2 <matchetator@gmail.com>
2023-06-25 16:45:26 +0200

    Add reset button

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let pixelBoard = document.querySelector('.pixel-board');
+const pixelBoard = document.querySelector('.pixel-board');
+const resetButton = document.querySelector('#reset-button');
+
 const pixelBoardSize = 960;
 let isMouseDown = false;
 
+resetButton.addEventListener('click', resetPixelBoard)
 document.body.onmousedown = () => {isMouseDown = true;};
 document.body.onmouseup = () => {isMouseDown = false;};
 
-createPixelBoard(10);
-//window.addEventListener('DOMContentLoaded', generatePixelBoard(10));
-//window.addEventListener('DOMContentLoaded', alert("damn son"));
+createPixelBoard(30);
+
+function resetPixelBoard(){
+    let resolution = prompt("Choose board size.");
+    createPixelBoard(resolution);
+}
 
 function createPixelBoard(resolution){
+    let pixelRow;
+
+    pixelBoard.innerHTML = "";
+
     for(let i = 0; i < resolution; i++){
+        pixelRow = document.createElement('div');
+        pixelRow.classList.add('pixel-row');
+        pixelBoard.appendChild(pixelRow);
+
         for(let j = 0; j < resolution; j++){
             createPixel(Math.floor(pixelBoardSize/resolution));
         }
@@ -19,13 +33,14 @@ function createPixelBoard(resolution){
     function createPixel(size){
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
-        pixel.style.width = size + "px";
-        pixel.style.height = size + "px";
         pixel.setAttribute('draggable', 'false');
+        pixel.addEventListener('mousedown', () => {
+            pixel.style.backgroundColor = "black";
+        }); 
         pixel.addEventListener('mouseover', () => {
             if(isMouseDown)
             pixel.style.backgroundColor = "black";
-        })
-        pixelBoard.appendChild(pixel);
+        });
+        pixelRow.appendChild(pixel);
     }
 }
